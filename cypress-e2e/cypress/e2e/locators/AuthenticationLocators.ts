a864908f0ec83478aaeba3b528529de5109603a3
Mousa Aricat <mousa.aricat@gmail.com>
2026-02-18 10:18:20 +0300

    Creating the locators for the first scenario

---
+++ cypress-e2e/cypress/e2e/locators/AuthenticationLocators.ts
@@ -0,0 +1,9 @@
+enum AuthenticationLocators{
+
+    EMAIL_FIELD_LOCATOR = "[data-e2e='email-field']",
+    PASSWORD_FIELD_LOCATOR = "[data-e2e='password-field']",
+    LOGIN_BUTTON_LOCATOR = "[data-e2e='submit-button']"
+
+}
+
+export default AuthenticationLocators
